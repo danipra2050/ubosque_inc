9427bab2700720cafd74cd11d7d58f7aa07d3311
Daniela Prado <dpradoc@unbosque.edu.co>
2021-10-20 21:53:21 -0500

    commit inicial

---
+++ src/main/java/com/ubosque/api/ubosqueapi/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.ubosque.api.ubosqueapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
